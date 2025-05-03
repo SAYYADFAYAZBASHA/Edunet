4bdb47388cad50eed5d05ed20745604ec616c12c
SAYYAD FAYAZ BASHA <fayaz1234basha@gmail.com>
2025-05-03 22:45:18 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,22 @@
+import pandas as pd
+
+def load_data(file_path):
+    try:
+        df = pd.read_csv(file_path)
+        return df
+    except FileNotFoundError:
+        return None
+
+def explore_data(df):
+    if df is not None:
+        print(df.head())
+        print(df.info())
+        print(df.describe())
+        print(df['Churn'].value_counts())
+
+if __name__ == "__main__":
+    data_file = 'customer_churn.csv'
+    churn_df = load_data(data_file)
+    if churn_df is not None:
+        explore_data(churn_df)
 
